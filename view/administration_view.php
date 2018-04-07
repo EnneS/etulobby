c661421990c4b5ae3d08869c414d4326131d2d55
Nerdzi <gilet.theophile@gmail.com>
2018-04-07 02:58:43 +0200

    ajout css administration

--- view/administration_view.php
+++ view/administration_view.php
@@ -17,7 +17,6 @@ if(isset($data["result"])) {
             <li><a href="afficherModules.php">Mes cours</a></li>
             <li><a href="afficherInformations.php">Mes informations</a></li>
             <li class="active"><a href="afficherAdministration.php">Administration</a></li>
-
         </ul>
         <ul class="right hide-on-med-and-down">
             <li><a href="deconnexion.php?logout='true'">Déconnexion</a></li>
@@ -27,37 +26,42 @@ if(isset($data["result"])) {
 </nav>
 
 <div class="container">
-  <div class="row center">
-      <h3 style="font-weight: bold;">Panel d'Administration</h3>
-  </div>
 
-  <div class="row">
-      <form class="col s12" method="post" action="afficherAdministration.php">
+    <div class="row">
+        <div class="row center" style="padding-top: 20px; margin:2px;">
+            <h4 style="font-weight: 500;">Panel administration</h4>
+        </div>
+    </div>
 
-          <div class="row center-align">
-                <div class="row">
+    <div class="divider" style="margin-bottom: 30px;"></div>
 
-                  <div class="input-field col s8 offset-s2">
-                      <input id="titre" name="titre" type="text" class="validate">
-                      <label class="active" for="nom">Titre</label>
-                  </div>
+    <div class="row">
+        <div class="col l8 offset-l2"
+            <div class="row card-panel grey lighten-4">
 
-                  <div class="input-field col s8 offset-s2">
-                    <textarea id="message" name="message" class="materialize-textarea"></textarea>
-                    <label for="textarea1">Message</label>
-                  </div>
-                </div>
+                <form method="post" action="afficherAdministration.php">
+                    <div class="row center-align">
+                        <div class="input-field col s8 offset-s2">
+                            <input id="titre" name="titre" type="text" class="validate">
+                            <label class="active" for="nom">Titre</label>
+                        </div>
+                        <div class="input-field col s8 offset-s2">
+                            <textarea id="message" name="message" class="materialize-textarea"></textarea>
+                            <label for="textarea1">Message</label>
+                        </div>
+                        <div class="center-align">
+                            <button class="btn waves-effect waves-light grey darken-4" type="submit" name="valider">Envoyer
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
 
-                <div class="center-align">
-                    <button class="btn waves-effect waves-light grey darken-4" type="submit" name="valider">Envoyer
-                        <i class="material-icons right">send</i>
-                    </button>
-                </div>
+            </div>
+        </div>
+    </div>
 
-              </form>
-
-          </div>
-  </div>
 </div>
+
 
 <?php include("../view/inc/footer.php"); ?>
